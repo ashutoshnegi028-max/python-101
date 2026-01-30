8c24cd47be8527b707099368f715913ea2263b61
ashutoshnegi028-max <ashutoshnegi028@gmail.com>
2026-01-30 15:28:42 +0530

    Fix loop range for 99 Bottles of Beer
    only change in the first line 
    for i in range(99, 98, -1):

--- 4-loops/20_99_bottles.py
+++ 4-loops/20_99_bottles.py
@@ -1,7 +1,7 @@
 # 99 Bottles of Beer 🍻
 # Codédex
 
-for i in range(99, 0, -1):
+for i in range(99, 98, -1):
   print(f'{i} bottles of beer on the wall')
   print(f'{i} bottles of beer')
   print('Take one down, pass it around')
